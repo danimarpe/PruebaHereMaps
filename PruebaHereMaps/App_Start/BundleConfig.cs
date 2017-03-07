2af4a2133dcabec8b21fb931006572da70d36a7b
Daniel <danimarpe@gmail.com>
2017-03-07 13:31:15 +0100

    Se crea la solución y se añaden los scripts de HereMaps

---
+++ PruebaHereMaps/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PruebaHereMaps
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                   "~/Content/Scripts/HereMaps/mapsjs-core.js",
+                   "~/Content/Scripts/HereMaps/mapsjs-service.js",
+                   "~/Content/Scripts/HereMaps/mapsjs-ui.js",
+                   "~/Content/Scripts/HereMaps/mapsjs-mapevents.js"
+                   ));
+
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+}
